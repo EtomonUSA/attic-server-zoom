a10613573ec14aa6e6e3870052224b946c1097d0
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-03-27 18:45:23 -0400

    v3, introduces better plugin system

--- src/Atticfile.ts
+++ src/Atticfile.ts
@@ -1,6 +1,3 @@
-import {
-    ApplicationContextBase
-} from '@znetstar/attic-server/lib/ApplicationContext';
 import { promises as fs } from 'fs';
 import {
     IIdentityEntity as
@@ -15,6 +12,7 @@ import { GenericError } from '@znetstar/attic-common/lib/Error/GenericError'
 import fetch from "node-fetch";
 import {IError} from "@znetstar/attic-common/lib/Error/IError";
 import {IIdentity} from "@znetstar/attic-common";
+import {IApplicationContext, IPlugin} from "@znetstar/attic-common/lib/Server";
 
 interface IIdentityEntityModel{
     externalId: string;
@@ -23,49 +21,60 @@ interface IIdentityEntityModel{
 
 type IIdentityEntity = IIdentityEntityModel&IIdentityEntityBase&IIdentity;
 
-export async function getZoomIdentityEntity(accessToken: IAccessToken): Promise<IIdentityEntity> {
-    let resp = await fetch(`https://api.zoom.us/v2/users/me`, {
-        headers: {
-            'Authorization': `Bearer ${accessToken.token}`
+
+export class AtticServerZoom implements IPlugin {
+    constructor(public applicationContext: IApplicationContext) {
+
+    }
+
+    public async getZoomIdentity(accessToken: IAccessToken): Promise<IIdentityEntity> {
+        let resp = await fetch(`https://api.zoom.us/v2/users/me`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken.token}`
+            }
+        });
+
+
+
+        let body:  any;
+        let e2: any;
+        try { body = await resp.json(); }
+        catch (err) { e2 = err; }
+
+        if (resp.status !== 200) {
+            throw new GenericError(`Could not locate Zoom identity`, 2001, 403, (
+                body || e2
+            ) as any as IError);
         }
-    });
 
 
+        let fields: IIdentityEntity = {
+            firstName: body.first_name,
+            lastName: body.last_name,
+            email: body.email,
+            clientName: accessToken.clientName,
+            phone: body.phone_number,
+            otherFields: body,
+            source: {
+                href: `https://api.zoom.us/v2/users/${body.id}`
+            },
+            type: 'IdentityEntity',
+            client: accessToken.client,
+            user: null,
+            externalId: body.id,
+            id: null,
+            _id: null
+        };
 
-    let body:  any;
-    let e2: any;
-    try { body = await resp.json(); }
-    catch (err) { e2 = err; }
-
-    if (resp.status !== 200) {
-        throw new GenericError(`Could not locate Zoom identity`, 2001, 403, (
-            body || e2
-        ) as any as IError);
+        return fields;
     }
 
 
-    let fields: IIdentityEntity = {
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        clientName: accessToken.clientName,
-        phone: body.phone_number,
-        otherFields: body,
-        source: {
-            href: `https://api.zoom.us/v2/users/${body.id}`
-        },
-        type: 'IdentityEntity',
-        client: accessToken.client,
-        user: null,
-        externalId: body.id,
-        id: null,
-        _id: null
-    };
+    public async init(): Promise<void> {
+        this.applicationContext.registerHook<IIdentityEntity>(`Client.getIdentityEntity.zoom.provider`, this.getZoomIdentity);
+    }
 
-    return fields;
-}
-
-
-export async function init(ctx: ApplicationContextBase) {
-    ctx.on(`Client.getIdentityEntity.zoom.provider`, getZoomIdentityEntity);
+    public get name(): string {
+        return JSON.parse((require('fs').readFileSync(require('path').join(__dirname, '..', 'package.json'), 'utf8'))).name;
+    }
 }
